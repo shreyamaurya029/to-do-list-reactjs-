5a41dae83012bbdbedfa0550fbf8a244f1aa88ec
Shreya Maurya <100403765+shreyamaurya029@users.noreply.github.com>
2023-07-30 16:33:29 +0530

    Update AddTaskForm.jsx

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,31 +1,25 @@
-const AddTaskForm = ({ newTask,addTask, setNewTask}) => {
-    return(
-        <>
-        {/*Add Task */}
-
-     <div className="row">
-       <div className="col">
-
-         <input
-         value={newTask}
-         onChange={ (e) => setNewTask(e.target.value)}
-          className="form-control form-control-lg"
-           />
-       </div>
-
-
-       <div className="col-auto">
-
-         <button 
-         className="btn btn-lg btn-success"
-         onClick={addTask}>
-           Add Task
-         </button>
-       </div>
-     </div>
-
-       </>
-    )
+const AddTaskForm = ({ newTask, setNewTask, addTask }) => {
+  return(
+    <>
+      {/* Add Task */}
+      <div className="row">
+        <div className="col">
+          <input 
+            value={newTask}
+            onChange={ (e) => setNewTask(e.target.value)}
+            className="form-control form-control-lg"
+          />
+        </div>
+        <div className="col-auto">
+          <button
+            onClick={addTask}
+            className="btn btn-lg btn-success"
+          >Add Task</button>
+        </div>
+      </div>
+      <br />
+    </>
+  )
 }
 
 export default AddTaskForm;
